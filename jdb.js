4c1b328a38e521533762f5e3355d2e1299429b4e
rsteele <rsteele@jdbyrider.com>
2017-03-21 11:15:31 -0400

    adding window onload

--- jdb.js
+++ jdb.js
@@ -1,6 +1,13 @@
-var selectedOptions = $(".dms-select")[0];
-if (selectedOptions) {
-	_.forEach(selectedOptions, function(option){
-		option.value = option.value + "test";
-	});
+window.onload = function() {
+	replaceOptionsListUrls();
 }
+
+
+var replaceOptionsListUrls = function() {
+	var selectedOptions = $(".dms-select")[0];
+	if (selectedOptions) {
+		_.forEach(selectedOptions, function(option){
+			option.value = option.value + "test";
+		});
+	};
+};

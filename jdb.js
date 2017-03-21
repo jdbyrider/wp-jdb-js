6167c3f1e9db5a4d655fa72d9ef546b8a10a7709
rsteele <rsteele@jdbyrider.com>
2017-03-21 15:46:49 -0400

    getting attribution value through js and creating attribution query string params

--- jdb.js
+++ jdb.js
@@ -1,11 +1,25 @@
 //run the scripts on page load
 window.onload = function() {
-	var attribution = $.url().param('attribution');
-	if (attribution){	
+	var attributionValue = getParameterByName("attribution");
+	if (attributionValue && attributionValue != ""){		
+		var attribution = $.param({ attribution:attributionValue });
 		replaceOptionsListUrls(attribution);
 		addQueryStringToHrefs(attribution);
 	}
 }
+
+//http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 
 //iterates through the options on the get me approved list and adds the visitor id to the query string parameters
 var addQueryStringToOptionsListUrls = function(attribution) {

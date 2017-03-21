c37ee66a4284de98a2ac67ccbd478bef9aa002cd
rsteele <rsteele@jdbyrider.com>
2017-03-21 10:42:25 -0400

    catch redirect

--- jdb.js
+++ jdb.js
@@ -1,1 +1,13 @@
 console.log('hi! it works');
+
+window.onbeforeunload = function (e) {
+    var e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Are you sure you want to leave the site?';
+    }
+
+    // For Safari
+    return 'Are you sure you want to leave the site?';
+};
